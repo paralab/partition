8c70d59e452222cdf6a31926423262158391d848
Budvin Chathura <chathura97rc@gmail.com>
2024-07-15 15:36:30 -0600

    Add results

--- misc/copy_meshes.sh
+++ misc/copy_meshes.sh
@@ -1,5 +1,5 @@
 # File containing list of mesh files
-file_list_file="/home/budvin/research/Partitioning/paralab-partition/misc/connected_meshes_tet.txt"
+file_list_file="/home/budvin/research/Partitioning/paralab-partition/misc/connected_meshes_hex.txt"
 
 # Read the file list into an array, skipping empty lines
 mapfile -t mesh_file_list < <(grep -v '^$' "$file_list_file")
@@ -9,5 +9,5 @@ mapfile -t mesh_file_list < <(grep -v '^$' "$file_list_file")
 for file_idx in {0..20}; do 
 
     echo "$file_idx copying file ${mesh_file_list[$file_idx]}"
-    scp ${mesh_file_list[$file_idx]} budvin@frontera.tacc.utexas.edu:/work2/10000/budvin/frontera/datasets/Meshes/10k_tet/
+    scp ${mesh_file_list[$file_idx]} budvin@frontera.tacc.utexas.edu:/work2/10000/budvin/frontera/datasets/Meshes/10k_hex/
 done
